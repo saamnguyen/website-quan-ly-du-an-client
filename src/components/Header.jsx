ad01d2e974781be12caa67c13a1d66d276b0fdc1
finn-01 <nguyensam576@gmail.com>
2022-04-27 22:08:52 +0700

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { Login } from "../pages";
+
 const Header = () => {
-    return (
-        <nav className="header">
-            <div className="left">
-                <div className="item menu">
-                    <i className="bx bxs-grid"></i>
-                </div>
-                <Link to={"/"}>
-                    <div className="item logo">
-                        <i className="bx bxs-buildings"></i>
-                        <h3>Trello</h3>
-                    </div>
-                </Link>
-            </div>
-            <div className="right">
-                <div className="search">
-                    <input type="text" placeholder="Tìm kiếm" />
-                    <i className="bx bx-search"></i>
-                </div>
-                <div className="item">
-                    <i className="bx bx-bell"></i>
-                </div>
-                <div className="item">
-                    <i className="bx bx-error-circle"></i>
-                </div>
-                <div className="account">
-                    <img
-                        src="https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/204612696_513858903381871_4463796854127515799_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_JkJDGptUwoAX9BvAlc&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_WsQ9FSJpcm8rPyqdKlaU1243bUBGUgbUFJTtJgIvxhQ&oe=62847331"
-                        alt=""
-                    />
-                </div>
-            </div>
-        </nav>
-    );
+	const [loggin, setLoggin] = useState(false);
+
+	const toggleLoggin = () => {
+		setLoggin(!loggin);
+		console.log(loggin);
+	};
+
+	return (
+		<nav className="header">
+			<div className="left">
+				<div className="item menu">
+					<i className="bx bxs-grid"></i>
+				</div>
+				<Link to={"/"}>
+					<div className="item logo">
+						<i className="bx bxs-buildings"></i>
+						<h3>Trello</h3>
+					</div>
+				</Link>
+			</div>
+			<div className="right">
+				<div className="search">
+					<input type="text" placeholder="Tìm kiếm" />
+					<i className="bx bx-search"></i>
+				</div>
+				<div className="item">
+					<i className="bx bx-bell"></i>
+				</div>
+				<div className="item">
+					<i className="bx bx-error-circle"></i>
+				</div>
+				<div className="account">
+					<img
+						src="https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/204612696_513858903381871_4463796854127515799_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_JkJDGptUwoAX9BvAlc&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_WsQ9FSJpcm8rPyqdKlaU1243bUBGUgbUFJTtJgIvxhQ&oe=62847331"
+						alt=""
+					/>
+					<div className="loggin">
+						<div className="loggin__components">
+							<p className="loggin__title">Account</p>
+						</div>
+						<div className="loggin__components loggin__profile">
+							<div className="loggin__profile-wrapper">
+								<img
+									className="loggin__profile_img"
+									src="https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/204612696_513858903381871_4463796854127515799_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=_JkJDGptUwoAX9BvAlc&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_WsQ9FSJpcm8rPyqdKlaU1243bUBGUgbUFJTtJgIvxhQ&oe=62847331"
+									alt="avatar"
+								/>
+								<div className="loggin__profile_title">
+									<p className="loggin__profile_title-name">
+										Nguyen Trung Hieu
+									</p>
+									<p className="loggin__profile_title-email">
+										trunghieu.dev@actvn.vn
+									</p>
+								</div>
+							</div>
+							<p className="loggin__add">Add another account</p>
+						</div>
+						<div className="loggin__components">
+							<a className="loggin__add">Profile and visibility</a>
+							<a className="loggin__add">Activity</a>
+							<a className="loggin__add">Cards</a>
+							<a className="loggin__add">Settings</a>
+						</div>
+						<div className="loggin__components">
+							<a className="loggin__add">Help</a>
+							<a className="loggin__add">Shortcuts</a>
+						</div>
+						<div className="loggin__components">
+							<Link
+								to={""}
+								className="loggin__add"
+								onClick={() => toggleLoggin()}
+							>
+								<a>Log out</a>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Login } from "./Login";
+export { default as Board } from "./Board";
+export { default as Home } from "./Home";
