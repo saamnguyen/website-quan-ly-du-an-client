cc52af04850e911be965e184cd84e775e1eedc9d
finn-01 <nguyensam576@gmail.com>
2022-05-03 23:18:57 +0700

    Home.jsx main

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Main = () => {
-	return <div>Main</div>;
+	return (
+		<div className="home__main">
+			<div className="home__main-body">
+				<div className="home__main-bg _3M1_NyFgE9sVvK"></div>
+				<div className="home__main-text">
+					<span>Stay on track and up to date</span>
+					<span>
+						Invite people to boards and cards, leave comments, add due dates,
+						and we'll show the most important activity here.
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Main;
